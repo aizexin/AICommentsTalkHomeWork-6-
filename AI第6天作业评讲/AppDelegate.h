0796a6b7f32cc43ce18b2753bb2280daf6a35f0a
aizexin <443792431@qq.com>
2015-09-23 09:11:36 +0800

    Initial Commit

---
+++ AI第6天作业评讲/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AI第6天作业评讲
+//
+//  Created by qianfeng on 15/9/23.
+//  Copyright (c) 2015年 aizexin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AI第6天作业评讲/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AI第6天作业评讲
+//
+//  Created by qianfeng on 15/9/23.
+//  Copyright (c) 2015年 aizexin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
